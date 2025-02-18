be725ceabf49df3e4fcc9d2d6586ab618c8a277d
Stephen <stephenfsmyth@gmail.com>
2025-02-18 11:27:55 -0400

    Move the StatusLabel in the client solution to a fixed location on the bottom centre of the screen

--- Client/scripts/Game.cs
+++ Client/scripts/Game.cs
@@ -33,7 +33,7 @@ public partial class Game : Node2D
         _tileMap = GetNode<TileMapLayer>("Ground");
         _player = GetNode<Sprite2D>("Player");
         _camera = GetNode<Camera2D>("Camera2D");
-        _statusLabel = GetNode<Label>("UI/StatusLabel");
+        _statusLabel = GetNode<Label>("Camera2D/StatusLabel");
         _network = GetNode<NetworkManager>("/root/NetworkManager");
         _gameState = GetNode<GameState>("/root/GameState");
 
